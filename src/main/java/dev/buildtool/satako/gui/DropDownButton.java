8e8c438a22fa13a7cf9e11e5a1b672c6109adba0
AlexiyOrlov <alexiy.ov@gmail.com>
2024-05-26 23:07:13 +0300

    Fix

--- src/main/java/dev/buildtool/satako/gui/DropDownButton.java
+++ src/main/java/dev/buildtool/satako/gui/DropDownButton.java
@@ -86,8 +86,8 @@ public class DropDownButton extends BetterButton {
     }
 
     @Override
-    protected void renderButton(DrawContext context, int mouseX, int mouseY, float delta) {
-        super.renderButton(context, mouseX, mouseY, delta);
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        super.render(context, mouseX, mouseY, delta);
         if (open)
             context.drawTextWithShadow(textRenderer, " :", getX() + width, getY() + height / 2 - 4, 0xffffffff);
         else
